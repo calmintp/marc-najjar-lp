4568abdb2bef90e9021c2dc68aa262c9e8ac27a6
paulo-sk <paulobsb1337@gmail.com>
2026-02-26 07:18:05 -0300

    feat: add filament for dashboard admin

---
+++ app/Filament/Resources/Books/Schemas/BookForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Books\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class BookForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                TextInput::make('subtitle'),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('amazon_link')
+                    ->required(),
+                FileUpload::make('image')
+                    ->image()
+                    ->required(),
+                Toggle::make('is_best_seller')
+                    ->required(),
+                TextInput::make('position')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+            ]);
+    }
+}
